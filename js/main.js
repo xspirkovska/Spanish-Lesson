76c7168441323e4108c0fb3094c864998ab3ff54
Nikolina Spirkovska <nspirkovska@icstars.org>
2017-06-15 14:44:36 -0500

    add main.js

--- js/main.js
+++ js/main.js
@@ -11,16 +11,16 @@ var MemoryGame = (function() {
       this.counter = document.querySelectorAll('.score .counter');
 
       $(MemoryGame.startButton).on('click', function() {
-          this.classList.add('hidden');
-          setup();
+        this.classList.add('hidden');
+        setup();
       });
     }
   }
 
   var setup = function() {
-      updateMarkup(MemoryGame.container);
-      addCards();
-      addEventListeners();
+    updateMarkup(MemoryGame.container);
+    addCards();
+    addEventListeners();
   }
 
   var addCards = function() {
@@ -32,78 +32,78 @@ var MemoryGame = (function() {
   }
 
   var revealAll = function() {
-      let cards = document.querySelectorAll('.card');
+    let cards = document.querySelectorAll('.card');
 
-      for(let card of cards) {
-          card.firstChild.classList.remove('hidden');
-      }
+    for(let card of cards) {
+      card.firstChild.classList.remove('hidden');
+    }
   }
 
   var addEventListeners = function() {
     let that = this;
 
     $(document).off().on('click', '.card' , function() {
-        let flippedCards = '';
+      let flippedCards = '';
 
-        toggleClass(this, 'flipped');
-        toggleClass(this.firstChild, 'hidden');
-        flippedCards = document.querySelectorAll('.flipped');
+      toggleClass(this, 'flipped');
+      toggleClass(this.firstChild, 'hidden');
+      flippedCards = document.querySelectorAll('.flipped');
 
-        if (flippedCards.length === 2) {
-            setTimeout(function () {
-                compare(flippedCards[0], flippedCards[1]);
-            }, 500);
-        }
+      if (flippedCards.length === 2) {
+        setTimeout(function () {
+          compare(flippedCards[0], flippedCards[1]);
+        }, 500);
+      }
     });
   }
 
   var compare = function(firstCard, lastCard) {
-      if (firstCard.innerHTML === lastCard.innerHTML) {
-          updateMarkup(firstCard);
-          updateMarkup(lastCard);
-          firstCard.classList.remove('flipped');
-          firstCard.classList.add('invisible');
-          lastCard.classList.remove('flipped');
-          lastCard.classList.add('invisible');
-          updateScore();
-      } else {
-          firstCard.classList.remove('flipped');
-          lastCard.classList.remove('flipped');
-          firstCard.firstChild.classList.add('hidden');
-          lastCard.firstChild.classList.add('hidden');
-      }
+    if (firstCard.innerHTML === lastCard.innerHTML) {
+      updateMarkup(firstCard);
+      updateMarkup(lastCard);
+      firstCard.classList.remove('flipped');
+      firstCard.classList.add('invisible');
+      lastCard.classList.remove('flipped');
+      lastCard.classList.add('invisible');
+      updateScore();
+    } else {
+      firstCard.classList.remove('flipped');
+      lastCard.classList.remove('flipped');
+      firstCard.firstChild.classList.add('hidden');
+      lastCard.firstChild.classList.add('hidden');
+    }
   }
 
   var updateMarkup = function(el, content) {
-      let newContent = content ? content : ''
-      el.innerHTML = newContent;
+    let newContent = content ? content : ''
+    el.innerHTML = newContent;
   }
 
   var updateScore = function() {
-      let $counter = $(MemoryGame.counter);
+    let $counter = $(MemoryGame.counter);
 
-      $counter.val(function(i, oldValue) {
-          return ++oldValue;
-      });
+    $counter.val(function(i, oldValue) {
+      return ++oldValue;
+    });
 
-      if ($counter.val() === '12') {
-        $counter.val(0);
-        restart();
-      }
+    if ($counter.val() === '12') {
+      $counter.val(0);
+      restart();
+    }
   }
 
   var toggleClass = function(el, className) {
-      if (!el.classList.contains(className)) {
-          el.classList.add(className);
-      } else {
-          el.classList.remove(className);
-      }
+    if (!el.classList.contains(className)) {
+      el.classList.add(className);
+    } else {
+      el.classList.remove(className);
+    }
   }
 
   var restart = function() {
-      updateMarkup(MemoryGame.startButton, 'RESTART GAME');
-      updateMarkup(MemoryGame.container, '<div class="centered modal">YOU WON!</div>');
-      MemoryGame.startButton.classList.remove('hidden');
+    updateMarkup(MemoryGame.startButton, 'RESTART GAME');
+    updateMarkup(MemoryGame.container, '<div class="centered modal">YOU WON!</div>');
+    MemoryGame.startButton.classList.remove('hidden');
   }
 
   return {
